df901c44d92e3a15cc9b2002a8b72b04ec4f4c31
Daniel <vedmack@gmail.com>
2013-03-16 23:07:14 +0200

    Update jquery.dataTables.yadcf.js

--- jquery.dataTables.yadcf.js
+++ jquery.dataTables.yadcf.js
@@ -19,15 +19,15 @@
 * 
 * Parameters:
 * 
-* Name				Required		Type		Default value		Possible values			Description
+* Name						Required		Type		Default value		Possible values			Description
 * ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-* column_number			true			String									The number of the column to which the filter will be applied
-* data				false			Array									When the need of predefined data for filter is needed just use an array ["value1","value2"....] 
-* column_data_type		false			String		text			text / html			The type of data in column , use "html" when you have some html code in the column (support parsing of multiple elements)
-* html_data_type		false			String		text			text / value / id		When using "html" for column_data_type argument you can choose how exactly to parse your html element/s in column , for example use "text" for the following <span class="someClass">Some text</span>
-* filter_container_id		false			String									In case that user don't want to place the filter in column header , he can pass an id of the desired container for the column filter 
-* filter_default_label		false			String		Select value						The label that will appear in the select menu filter when no value is selected from the filter
-* filter_reset_button_text	false			String		x							The text that will appear inside the reset button next to the select drop down
+* column_number				true			String													The number of the column to which the filter will be applied
+* data						false			Array													When the need of predefined data for filter is needed just use an array ["value1","value2"....] 
+* column_data_type			false			String		text				text / html				The type of data in column , use "html" when you have some html code in the column (support parsing of multiple elements)
+* html_data_type			false			String		text				text / value / id		When using "html" for column_data_type argument you can choose how exactly to parse your html element/s in column , for example use "text" for the following <span class="someClass">Some text</span>
+* filter_container_id		false			String													In case that user don't want to place the filter in column header , he can pass an id of the desired container for the column filter 
+* filter_default_label		false			String		Select value								The label that will appear in the select menu filter when no value is selected from the filter
+* filter_reset_button_text	false			String		x											The text that will appear inside the reset button next to the select drop down
 *
 *
 *
