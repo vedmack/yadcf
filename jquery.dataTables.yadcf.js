6de0e6146256d4a4556f1d7d1002798ef5e0302a
Daniel <vedmack@gmail.com>
2013-03-17 22:04:41 +0200

    Update jquery.dataTables.yadcf.js
    changed the docs table

--- jquery.dataTables.yadcf.js
+++ jquery.dataTables.yadcf.js
@@ -18,17 +18,55 @@
 * or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
 * 
 * Parameters:
-* 
-* Name						Required		Type		Default value		Possible values			Description
-* ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-* column_number				true			String													The number of the column to which the filter will be applied
-* data						false			Array													When the need of predefined data for filter is needed just use an array ["value1","value2"....] 
-* column_data_type			false			String		text				text / html				The type of data in column , use "html" when you have some html code in the column (support parsing of multiple elements)
-* text_data_delimiter		false			String													Delimiter that seperates text in table column, for example text_data_delimiter: ","
-* html_data_type			false			String		text				text / value / id		When using "html" for column_data_type argument you can choose how exactly to parse your html element/s in column , for example use "text" for the following <span class="someClass">Some text</span>
-* filter_container_id		false			String													In case that user don't want to place the filter in column header , he can pass an id of the desired container for the column filter 
-* filter_default_label		false			String		Select value								The label that will appear in the select menu filter when no value is selected from the filter
-* filter_reset_button_text	false			String		x											The text that will appear inside the reset button next to the select drop down
+*
+*					
+* -------------
+
+* column_number
+				Required:			true
+				Type:				String
+				Description:		The number of the column to which the filter will be applied
+				
+* data
+				Required:			false
+				Type:				Array
+				Description:		When the need of predefined data for filter is needed just use an array ["value1","value2"....] 
+					
+* column_data_type
+				Required:			false
+				Type:				String
+				Default value:		text
+				Possible values:	text / html	
+				Description:		The type of data in column , use "html" when you have some html code in the column (support parsing of multiple elements per cell)
+
+* text_data_delimiter
+				Required:			false
+				Type:				String
+				Description:		Delimiter that seperates text in table column, for example text_data_delimiter: ","
+										
+* html_data_type
+				Required:			false
+				Type:				String
+				Default value:		text
+				Possible values:	text / value / id			
+				Description:		When using "html" for column_data_type argument you can choose how exactly to parse your html element/s in column , for example use "text" for the following <span class="someClass">Some text</span>
+
+* filter_container_id
+				Required:			false
+				Type:				String
+				Description:		In case that user don't want to place the filter in column header , he can pass an id of the desired container for the column filter 
+		
+* filter_default_label
+				Required:			false
+				Type:				String
+				Default value:		Select value
+				Description:		The label that will appear in the select menu filter when no value is selected from the filter
+									
+* filter_reset_button_text
+				Required:			false
+				Type:				String
+				Default value:		x
+				Description:		The text that will appear inside the reset button next to the select drop down
 *
 *
 */
