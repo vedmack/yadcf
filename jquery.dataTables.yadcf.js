7845f107e14effe13ff2e2cc96dcf7bda11bffe5
Daniel <vedmack@gmail.com>
2013-04-02 23:17:15 +0300

    Update jquery.dataTables.yadcf.js
    set focus on select instead on reset button

--- jquery.dataTables.yadcf.js
+++ jquery.dataTables.yadcf.js
@@ -233,10 +233,9 @@ var yadcf = (function ($) {
 
 	function doFilter(filter_selector_string, arg, column_number) {
 		if (arg === "clear") {
-			$(filter_selector_string).find(".yadcf-filter").val("-1");
+			$(filter_selector_string).find(".yadcf-filter").val("-1").focus();
 			$(document).data(filter_selector_string + "_val", "-1");
 			oTable.fnFilter("", column_number);
-			$(filter_selector_string).find(".yadcf-filter-reset-button").focus();
 			return;
 		}
 
