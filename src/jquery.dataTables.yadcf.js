0b9d920b8458b0a66b95e4e3e23cf9e37e0a1cc0
vedmack <vedmack@gmail.com>
2018-07-25 09:50:38 +0300

    bug fix: https://github.com/vedmack/yadcf/issues/482 - Autocomplete is not reading filter_plugin_options

--- src/jquery.dataTables.yadcf.js
+++ src/jquery.dataTables.yadcf.js
@@ -2,7 +2,7 @@
 * Yet Another DataTables Column Filter - (yadcf)
 *
 * File:        jquery.dataTables.yadcf.js
-* Version:     0.9.4.beta.3
+* Version:     0.9.4.beta.3 
 *
 * Author:      Daniel Reznick
 * Info:        https://github.com/vedmack/yadcf
