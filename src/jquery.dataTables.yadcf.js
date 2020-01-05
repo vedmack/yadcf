b99f6ddee81fa79ba86a5147dbd804a8fa0bcf7b
vedmack <vedmack@gmail.com>
2020-01-05 17:04:03 +0200

    0.9.4.beta.37
    bug fix in dTXhrComplete

--- src/jquery.dataTables.yadcf.js
+++ src/jquery.dataTables.yadcf.js
@@ -2,7 +2,7 @@
 * Yet Another DataTables Column Filter - (yadcf)
 *
 * File:        jquery.dataTables.yadcf.js
-* Version:     0.9.4.beta.36
+* Version:     0.9.4.beta.37
 *
 * Author:      Daniel Reznick
 * Info:        https://github.com/vedmack/yadcf
@@ -4688,6 +4688,7 @@ if (!Object.entries) {
 						if (dTXhrComplete !== undefined ) {
 							yadcfDelay(function () {
 								dTXhrComplete();
+								dTXhrComplete = undefined;
 							}, 100);
 						}
 					});
